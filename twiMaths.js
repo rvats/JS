0c74b1e59e7190f969e25aee7211ced48992d66c
Rahul Vats <vats.rahul@gmail.com>
2017-04-02 10:41:03 -0500

    Add files via upload

--- twiMaths.js
+++ twiMaths.js
@@ -3,3 +3,30 @@ function WriteTable (number) {
         console.log(number + ' * ' + i + ' = ' + number*i);
     }
 }
+
+function SieveOfEratosthenes(N) {
+    var arrayOfPrimeNumbers = [];
+    for(var i = 0; i < parseInt(N); i++) {
+        if(i < 2) {
+            arrayOfPrimeNumbers[i] = false;
+        } else {
+            arrayOfPrimeNumbers[i] = true;
+        }
+    }
+    for (var p=2; p*p<=N; p++)
+    {
+        if (arrayOfPrimeNumbers[p] == true)
+        {
+            for (var i=p*2; i<=N; i += p)
+                arrayOfPrimeNumbers[i] = false;
+        }
+    }
+    for (var p=2; p<=N; p++) {
+       if (arrayOfPrimeNumbers[p]) {
+           arrayOfPrimeNumbers[p] = p;
+       } 
+    }
+	arrayOfPrimeNumbers = arrayOfPrimeNumbers.remove(false);
+    return arrayOfPrimeNumbers;
+}
+
